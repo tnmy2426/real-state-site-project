09b7fbaaa58315c914878eb9ba22842807ef1d66
Abdullah Al Nahdi <al.nahdi.tnmy@gmail.com>
2020-11-23 01:16:48 +0600

    Installed react-router-dom, react-redux, redux, redux-thunk, redux-devtools-extensions. Also created components and containers and other necessary folder for frontend. And configured urls routing with react-router-dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,32 @@
+import React from 'react';
+import Layout from './HighOrderComponents/Layout';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './containers/Home';
+import About from './containers/About';
+import Listings from './containers/Listings';
+import ListingDetail from './containers/ListingDetail';
+import SignUp from './containers/SignUp';
+import Signin from './containers/Signin';
+import NotFound from './components/NotFound';
 
-import './App.css';
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <h1>Hello!</h1>
-    </div>
-  );
-}
+    <Router>
+      <Layout>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/about' component={About} />
+          <Route exact path='/listings' component={Listings} />
+          <Route exact path='/listings/:id' component={ListingDetail} />
+          <Route exact path='/signup' component={SignUp} />
+          <Route exact path='/signin' component={Signin} />
+          <Route component={NotFound} />
+        </Switch>
+      </Layout>
+    </Router>
 
-export default App;
+  )
+};
+
+export default App
+
---
+++ frontend/src/containers/ListingDetail.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ListingDetail = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default ListingDetail
