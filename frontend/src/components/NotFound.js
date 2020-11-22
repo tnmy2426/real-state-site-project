9f6db1224ff70c4debcebd036f628deaf0242413
Abdullah Al Nahdi <al.nahdi.tnmy@gmail.com>
2020-11-23 03:04:08 +0600

    NotFound Page Designed.Fornt-end done.

--- frontend/src/components/NotFound.js
+++ frontend/src/components/NotFound.js
@@ -2,8 +2,9 @@ import React from 'react'
 
 const NotFound = () => {
     return (
-        <div>
-            
+        <div className="not_found_page ">
+            <h1 className='notFound_heading'>404 Not Found 😢</h1>
+            <p className='notFound_paragraph'>The link you requested does not exist in this site.</p>
         </div>
     )
 }
--- frontend/src/containers/ListingDetail.js
+++ frontend/src/containers/ListingDetail.js
@@ -3,7 +3,7 @@ import React from 'react'
 const ListingDetail = () => {
     return (
         <div>
-            
+            ListingDetail
         </div>
     )
 }
